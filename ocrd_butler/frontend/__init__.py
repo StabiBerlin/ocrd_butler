093bafc4952e2b3289161094e06feee2368299e0
Marco Scheidhuber <marco.scheidhuber@sbb.spk-berlin.de>
2019-10-31 10:25:36 +0100

    Add download methods.

--- ocrd_butler/frontend/__init__.py
+++ ocrd_butler/frontend/__init__.py
@@ -1,14 +1,23 @@
+import os
 import json
+import glob
+import zipfile
+import io
+import pathlib
+
+from flask import Blueprint, render_template, flash, redirect, url_for, jsonify, Response, send_file
+
+import xml.etree.ElementTree as ET
 
 from ocrd_butler import celery
-
-from flask import Blueprint, render_template, flash, redirect, url_for, jsonify
 
 from ocrd_butler.frontend.nav import nav
 from ocrd_butler.api.processors import PROCESSORS
 
 from ocrd_butler.database.models import Chain as db_model_Chain
 from ocrd_butler.database.models import Task as db_model_Task
+
+from ocrd_butler.util import get_config_json
 
 
 frontend = Blueprint('frontend', __name__)
@@ -77,7 +86,68 @@ def tasks():
 
         current_tasks.append(task)
 
-
     return render_template(
         'tasks.html',
         tasks=current_tasks)
+
+
+@frontend.route('/download/txt/<string:worker_id>')
+def download(worker_id):
+    # TODO: we have to have the result (destination) dir in the result of the worker
+    res = celery.AsyncResult(worker_id)
+    config_json = get_config_json()
+    dst_dir = "{}/{}".format(config_json["OCRD_BUTLER_RESULTS"], res.result["task_id"])
+    page_xml_dir = os.path.join(dst_dir, "RECOGNIZE")
+
+    fulltext = ""
+
+    namespace = {'page_2009-03-16': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2009-03-16',
+                 'page_2010-01-12': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-01-12',
+                 'page_2010-03-19': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-03-19',
+                 'page_2013-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15',
+                 'page_2016-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2016-07-15',
+                 'page_2017-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15',
+                 'page_2018-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2018-07-15',
+                 'page_2019-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15'}
+
+    for file in glob.glob("{}/*.xml".format(page_xml_dir)):
+        tree = ET.parse(file)
+        xmlns = tree.getroot().tag.split("}")[0].strip("{")
+        if xmlns in namespace.values():
+            for regions in tree.iterfind(".//{%s}TextEquiv" % xmlns):
+                fulltext += "\n"
+                for region in regions.findall("{%s}Unicode" % xmlns):
+                    if region.text is not None:
+                        fulltext += region.text
+
+    return Response(
+        fulltext,
+        mimetype="text/txt",
+        headers={
+            "Content-Disposition":
+            "attachment;filename=fulltext_%s.txt" % res.result["task_id"]
+        }
+    )
+
+
+@frontend.route('/download/xml/<string:worker_id>')
+def download_zip(worker_id):
+    # TODO: we have to have the result (destination) dir in the result of the worker
+    res = celery.AsyncResult(worker_id)
+    config_json = get_config_json()
+    dst_dir = "{}/{}".format(config_json["OCRD_BUTLER_RESULTS"], res.result["task_id"])
+    page_xml_dir = os.path.join(dst_dir, "RECOGNIZE")
+    base_path = pathlib.Path(page_xml_dir)
+
+    data = io.BytesIO()
+    with zipfile.ZipFile(data, mode='w') as z:
+        for f_name in base_path.iterdir():
+            z.write(f_name)
+    data.seek(0)
+
+    return send_file(
+        data,
+        mimetype='application/zip',
+        as_attachment=True,
+        attachment_filename="ocr_page_xml_%s.zip" % res.result["task_id"]
+    )
