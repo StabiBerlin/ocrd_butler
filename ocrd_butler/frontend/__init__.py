ee0b8f92cd59e707dbec7a1a9083d1be3988ec10
Marco Scheidhuber <marco.scheidhuber@sbb.spk-berlin.de>
2019-11-11 15:35:48 +0100

    Coding styles.

--- ocrd_butler/frontend/__init__.py
+++ ocrd_butler/frontend/__init__.py
@@ -18,48 +18,54 @@ from ocrd_butler.database.models import Chain as db_model_Chain
 from ocrd_butler.database.models import Task as db_model_Task
 
 
-frontend = Blueprint('frontend', __name__)
+frontend = Blueprint("frontend", __name__)
 
-@frontend.route('/')
+@frontend.route("/")
 def index():
-    return render_template('index.html')
+    """Define our index page."""
+    return render_template("index.html")
 
 @frontend.app_errorhandler(404)
 def not_found(err):
+    """Define our custom 404 page."""
     return render_template("404.html", error=err)
 
 @frontend.app_errorhandler(500)
 def handle_500(err):
+    """Define our custom 500 page."""
     import traceback
     return render_template(
-        '500.html',
+        "500.html",
         error=err,
         exception=err.original_exception,
         traceback=traceback.format_exc()), 500
 
-@frontend.route('/processors')
+@frontend.route("/processors")
 def processors():
+    """Define the page presenting the integrated processors."""
     return render_template(
-        'processors.html',
+        "processors.html",
         processors=PROCESSORS)
 
-@frontend.route('/chains')
+@frontend.route("/chains")
 def chains():
+    """Define the page presenting the configured chains."""
     results = db_model_Chain.query.all()
 
     current_chains = [{
-            "id": c.id,
-            "name": c.name,
-            "description": c.description,
-            "processors": json.loads(c.processors)
+        "id": c.id,
+        "name": c.name,
+        "description": c.description,
+        "processors": json.loads(c.processors)
         } for c in results]
 
     return render_template(
-        'chains.html',
+        "chains.html",
         chains=current_chains)
 
 @frontend.route('/tasks')
 def tasks():
+    """Define the page presenting the created tasks."""
     results = db_model_Task.query.all()
 
     current_tasks = []
@@ -86,27 +92,29 @@ def tasks():
         current_tasks.append(task)
 
     return render_template(
-        'tasks.html',
+        "tasks.html",
         tasks=current_tasks)
 
 
-@frontend.route('/download/txt/<string:worker_id>')
-def download(worker_id):
+@frontend.route("/download/txt/<string:worker_id>")
+def download_txt(worker_id):
+    """Define route to download the results as text."""
     # TODO: we have to have the result (destination) dir in the result of the worker
     res = celery.AsyncResult(worker_id)
     dst_dir = "{}".format(res.result["result_dir"])
     page_xml_dir = os.path.join(dst_dir, "OCRD-RECOGNIZE")
     fulltext = ""
 
-    namespace = {'page_2009-03-16': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2009-03-16',
-                 'page_2010-01-12': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-01-12',
-                 'page_2010-03-19': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-03-19',
-                 'page_2013-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15',
-                 'page_2016-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2016-07-15',
-                 'page_2017-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15',
-                 'page_2018-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2018-07-15',
-                 'page_2019-07-15': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15'}
-
+    namespace = {
+        "page_2009-03-16": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2009-03-16",
+        "page_2010-01-12": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-01-12",
+        "page_2010-03-19": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2010-03-19",
+        "page_2013-07-15": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15",
+        "page_2016-07-15": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2016-07-15",
+        "page_2017-07-15": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15",
+        "page_2018-07-15": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2018-07-15",
+        "page_2019-07-15": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2019-07-15"
+    }
     for file in glob.glob("{}/*.xml".format(page_xml_dir)):
         tree = ET.parse(file)
         xmlns = tree.getroot().tag.split("}")[0].strip("{")
@@ -127,8 +135,9 @@ def download(worker_id):
     )
 
 
-@frontend.route('/download/xml/<string:worker_id>')
-def download_zip(worker_id):
+@frontend.route("/download/xml/<string:worker_id>")
+def download_xml_zip(worker_id):
+    """Define route to download the page xml results as zip file."""
     # TODO: we have to have the result (destination) dir in the result of the worker
     res = celery.AsyncResult(worker_id)
     dst_dir = "{}".format(res.result["result_dir"])
@@ -143,7 +152,7 @@ def download_zip(worker_id):
 
     return send_file(
         data,
-        mimetype='application/zip',
+        mimetype="application/zip",
         as_attachment=True,
         attachment_filename="ocr_page_xml_%s.zip" % res.result["task_id"]
     )
