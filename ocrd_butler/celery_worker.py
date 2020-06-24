dce784dca229d9f375c52d705967c47d6c490198
Marco Scheidhuber <marco.scheidhuber@sbb.spk-berlin.de>
2020-06-24 10:28:50 +0200

    No qa with flakes.

--- ocrd_butler/celery_worker.py
+++ ocrd_butler/celery_worker.py
@@ -6,6 +6,6 @@ from ocrd_butler import celery
 from ocrd_butler.app import flask_app
 from ocrd_butler.celery_utils import init_celery
 # next one is important to get tasks initialized
-from ocrd_butler.execution.tasks import run_task
+from ocrd_butler.execution.tasks import run_task  # noqa
 
 init_celery(celery, flask_app)
