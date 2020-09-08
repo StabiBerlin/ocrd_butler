5bf9266684d8475ce4ce583047b62efd55b453f1
Marco Scheidhuber <marco.scheidhuber@sbb.spk-berlin.de>
2020-09-08 17:16:49 +0200

    Refactore getting the processor information.

--- ocrd_butler/api/processors.py
+++ ocrd_butler/api/processors.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
-"""Our chain configuration and the predefined processor chains."""
+"""
+Extract predefined processors information.
+"""
 
-import os
 import json
 import copy
+import subprocess
 
 from flask import jsonify
 from flask_restx import Resource
 
 from ocrd_butler.api.restx import api
 
-# TODO: This should be a loadable conf in JSON or similiar
 from ocrd_butler.config import Config
 
 ocrd_config = Config()
@@ -20,69 +21,19 @@ processors_namespace = api.namespace(
     "processors",
     description="Get the processors known by our butler.")
 
-
 PROCESSORS_CONFIG = {}
 
-
-for package in ocrd_config.DIRECT_PROCESSOR_SCRIPTS:
-    ocrd_tool_file = os.path.abspath(os.path.join(package, "ocrd-tool.json"))
-
-    if not os.path.exists(ocrd_tool_file):
-        raise ImportError(
-            "Can't find ocrd-tools.json {0}, giving up.".format(ocrd_tool_file))
-
-    with open(ocrd_tool_file) as fh:
-        ocrd_tool = json.load(fh)
-
-    package_information = {"package": {"name": os.path.basename(package)}}
-    for name, value in ocrd_tool.items():
-        if name == "tools":
-            continue
-        package_information["package"][name] = value
-
-    for name, config in ocrd_tool["tools"].items():
-        PROCESSORS_CONFIG[name] = config
-        PROCESSORS_CONFIG[name].update(package_information)
-
-
-for package in ocrd_config.PROCESSOR_PACKAGES:
-    module = __import__(package, fromlist=["config", "cli"])
-    m_path = module.__path__[0]
-
-    ocrd_tool_file = os.path.abspath(os.path.join(m_path, "ocrd-tool.json"))
-    if not os.path.exists(ocrd_tool_file):
-        ocrd_tool_file = os.path.abspath(
-            os.path.join(m_path, "..", "ocrd-tool.json"))
-    if not os.path.exists(ocrd_tool_file):
-        # ocrd_keraslm
-        ocrd_tool_file = os.path.abspath(
-            os.path.join(m_path, "wrapper", "ocrd-tool.json"))
-    if not os.path.exists(ocrd_tool_file):
-        raise ImportError(
-            "Can't find ocrd-tools.json {0}, giving up.".format(ocrd_tool_file))
-
-    with open(ocrd_tool_file) as fh:
-        ocrd_tool = json.load(fh)
-
-    package_information = {"package": {"name": package}}
-    for name, value in ocrd_tool.items():
-        if name == "tools":
-            continue
-        package_information["package"][name] = value
-
-    for name, config in ocrd_tool["tools"].items():
-        PROCESSORS_CONFIG[name] = config
-        PROCESSORS_CONFIG[name].update(package_information)
-
+for package in ocrd_config.PROCESSORS:
+    ocrd_tool = subprocess.check_output([package, "--dump-json"])
+    PROCESSORS_CONFIG[package] = {"package": {"name": package}}
+    PROCESSORS_CONFIG[package].update(json.loads(ocrd_tool))
 
 PROCESSOR_NAMES = PROCESSORS_CONFIG.keys()
-
 
 # We prepare an usable action configuration from the config itself.
 PROCESSORS_ACTION = copy.deepcopy(PROCESSORS_CONFIG)
 for name, config in PROCESSORS_ACTION.items():
 
-    # TODO: check why not every processor has the package information
     if "package" in config:
         del config["package"]
 
--- ocrd_butler/config.py
+++ ocrd_butler/config.py
@@ -1,38 +1,118 @@
-""" Default configuration for the butler.
-    https://flask.palletsprojects.com/en/1.1.x/config/
+"""
+Default configuration for the butler.
+https://flask.palletsprojects.com/en/1.1.x/config/
 """
 
 
 class Config(object):
-    """Base config, uses staging database server."""
+    """
+    Base config, uses staging database server.
+    """
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = None
     CELERY_RESULT_BACKEND_URL = "redis://localhost:6379"
     CELERY_BROKER_URL = "redis://localhost:6379"
     OCRD_BUTLER_RESULTS = "/tmp/ocrd_butler_results"
-    DIRECT_PROCESSOR_SCRIPTS = [
-        "/srv/ocrd_all/ocrd_olena",
-        "/srv/ocrd_all/dinglehopper",
-        "/srv/ocrd_all/sbb_textline_detector",
-        "/srv/ocrd_all/ocrd_fileformat"
-    ]
-    PROCESSOR_PACKAGES = [
-        "ocrd_tesserocr",
-        "ocrd_calamari",
-        "ocrd_segment",
-        "ocrd_keraslm",
-        "ocrd_anybaseocr"
+    PROCESSORS = [
+        "ocrd-calamari-recognize",
+
+        "ocrd-olena-binarize",
+
+        "ocrd-sbb-textline-detector",
+
+        "ocrd-fileformat-transform",
+
+        "ocrd-tesserocr-binarize",
+        "ocrd-tesserocr-recognize",
+        "ocrd-tesserocr-segment-table",
+        "ocrd-tesserocr-crop",
+        "ocrd-tesserocr-segment-line",
+        "ocrd-tesserocr-segment-word",
+        "ocrd-tesserocr-deskew",
+        "ocrd-tesserocr-segment-region",
+
+        "ocrd-keraslm-rate",
+
+        "ocrd-segment-evaluate",
+        "ocrd-segment-extract-regions",
+        "ocrd-segment-repair",
+        "ocrd-segment-extract-lines",
+        "ocrd-segment-from-coco",
+        "ocrd-segment-replace-original",
+        "ocrd-segment-extract-pages",
+        "ocrd-segment-from-masks",
+
+        # "ocrd-anybaseocr-binarize",
+        # "ocrd-anybaseocr-dewarp",
+        # "ocrd-anybaseocr-block-segmentation",
+        # "ocrd-anybaseocr-layout-analysis",
+        # "ocrd-anybaseocr-crop",
+        # "ocrd-anybaseocr-textline",
+        # "ocrd-anybaseocr-deskew",
+        # "ocrd-anybaseocr-tiseg",
+
+        "ocrd-dinglehopper",
+
+        "ocrd-pagetopdf",
+
+        # "ocrd-make",
+
+        "ocrd-pc-segmentation",
+
+        "ocrd-preprocess-image",
+
+        "ocrd-repair-inconsistencies",
+
+        # "ocrd-cis-align",
+        # "ocrd-cis-data",
+        # "ocrd-cis-ocropy-binarize",
+        # "ocrd-cis-ocropy-clip",
+        # "ocrd-cis-ocropy-denoise",
+        # "ocrd-cis-ocropy-deskew",
+        # "ocrd-cis-ocropy-dewarp",
+        # "ocrd-cis-ocropy-rec",
+        # "ocrd-cis-ocropy-recognize",
+        # "ocrd-cis-ocropy-resegment",
+        # "ocrd-cis-ocropy-segment",
+        # "ocrd-cis-ocropy-train",
+        # "ocrd-cis-postcorrect",
+
+        # "ocrd-cor-asv-ann-evaluate",
+        # "ocrd-cor-asv-ann-process",
+
+        # "ocrd-dummy",
+
+        # "ocrd-export-larex",
+
+        # "ocrd-im6convert",
+
+        # "ocrd-typegroups-classifier",
+
+        # "ocrd-import",
+
+        # "ocrd-skimage-binarize",
+        # "ocrd-skimage-denoise",
+        # "ocrd-skimage-denoise-raw",
+        # "ocrd-skimage-normalize",
     ]
 
 class ProductionConfig(Config):
-    """Uses production database server."""
+    """
+    Uses production database server.
+    """
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./production.db'
 
 class DevelopmentConfig(Config):
+    """
+    Uses development database server.
+    """
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./development.db'
 
 class TestingConfig(Config):
+    """
+    Uses in memory database for testing.
+    """
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     OCRD_BUTLER_RESULTS = "/tmp/ocrd_butler_results_testing"
