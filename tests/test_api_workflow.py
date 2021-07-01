9bbb1e75a8afdb7079f1e47cfc1fd25f0a302c27
Höper, Jakob <jakob.hoeper@sbb.spk-berlin.de>
2021-07-01 12:31:57 +0200

    [#12748] rename test module

--- tests/test_api_workflow.py
+++ tests/test_api_workflow.py
@@ -26,37 +26,37 @@ class ApiTests(TestCase):
     def create_app(self):
         return create_app(config=TestingConfig)
 
-    def test_create_new_chain(self):
-        """Check if a new chain is created."""
+    def test_create_new_workflow(self):
+        """Check if a new workflow is created."""
         response = self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-tesserocr-recognize"],
         ))
         assert response.status_code == 201
         assert response.json["message"] == "Workflow created."
         assert response.json["id"] is not None
 
-    def test_create_chain_with_unknown_processor(self):
-        """Check if a new chain is created."""
+    def test_create_workflow_with_unknown_processor(self):
+        """Check if a new workflow is created."""
         response = self.client.post("/api/workflows", json=dict(
-            name="chain", processors=["foobar"]))
+            name="workflow", processors=["foobar"]))
         assert response.status_code == 400
         assert response.json["message"] == "Wrong parameter."
         assert response.json["status"] == 'Unknown processor "foobar".'
 
-    def test_create_chain_without_processor(self):
-        """Check if a new chain is created."""
-        response = self.client.post("/api/workflows", json=dict(name="chain"))
+    def test_create_workflow_without_processor(self):
+        """Check if a new workflow is created."""
+        response = self.client.post("/api/workflows", json=dict(name="workflow"))
         assert response.status_code == 400
         assert response.json["message"] == "Wrong parameter."
         assert response.json["status"] == "Missing processors for workflow."
 
-    def test_create_chain_with_default_parameters(self):
-        """Check if a new chain is created."""
+    def test_create_workflow_with_default_parameters(self):
+        """Check if a new workflow is created."""
         self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-tesserocr-segment-region",
                         "ocrd-tesserocr-segment-line",
                         "ocrd-tesserocr-segment-word",
@@ -68,11 +68,11 @@ class ApiTests(TestCase):
         assert response.json["parameters"]["ocrd-tesserocr-recognize"]["overwrite_segments"] is False
         assert response.json["parameters"]["ocrd-tesserocr-segment-word"]["overwrite_words"] is True
 
-    def test_create_chain_with_own_parameters(self):
-        """Check if a new chain is created."""
+    def test_create_workflow_with_own_parameters(self):
+        """Check if a new workflow is created."""
         response = self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-olena-binarize"],
             parameters={
                 "ocrd-olena-binarize": {
@@ -85,11 +85,11 @@ class ApiTests(TestCase):
         assert "ocrd-olena-binarize" in response.json["parameters"].keys()
         assert response.json["parameters"]["ocrd-olena-binarize"]["impl"] == "sauvola-ms-split"
 
-    def test_create_chain_with_wrong_parameters(self):
-        """Check if a new chain is created."""
+    def test_create_workflow_with_wrong_parameters(self):
+        """Check if a new workflow is created."""
         response = self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-olena-binarize"],
             parameters={
                 "ocrd-olena-binarize": {
@@ -103,55 +103,55 @@ class ApiTests(TestCase):
         assert response.json["status"].startswith(
             "Error while validating parameters \"{\'impl\': \'foobar\',")
 
-    def test_get_chain(self):
-        """Check if an existing chain is returned."""
+    def test_get_workflow(self):
+        """Check if an existing workflow is returned."""
         response = self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-olena-binarize"],
         ))
         response = self.client.get("/api/workflows/1")
         assert response.status_code == 200
         assert response.json["id"] == 1
-        assert response.json["name"] == "New Chain"
-        assert response.json["description"] == "Some foobar chain."
+        assert response.json["name"] == "New Workflow"
+        assert response.json["description"] == "Some foobar workflow."
         assert "ocrd-olena-binarize" in response.json["processors"]
 
-    def test_delete_chain(self):
-        """Check if a new chain is created."""
+    def test_delete_workflow(self):
+        """Check if a new workflow is created."""
         response = self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-tesserocr-recognize"],
         ))
         response = self.client.delete("/api/workflows/1")
         assert response.status_code == 200
-        assert response.json["message"] == "Delete workflow \"New Chain(1)\": success"
+        assert response.json["message"] == "Delete workflow \"New Workflow(1)\": success"
         response = self.client.get("/api/workflows/1")
         assert response.status_code == 404
 
-    def test_delete_unknown_chain(self):
-        """Check if a new chain is created."""
+    def test_delete_unknown_workflow(self):
+        """Check if a new workflow is created."""
         response = self.client.delete("/api/workflows/13")
         assert response.status_code == 404
         assert response.json["status"] == "Can't find a workflow with the id \"13\"."
 
-    def test_get_unknown_chain(self):
-        """Check if a non existing chain ...."""
+    def test_get_unknown_workflow(self):
+        """Check if a non existing workflow ...."""
         response = self.client.get("/api/workflows/23")
         assert response.status_code == 404
         assert response.json["status"] == "Can't find a workflow with the id \"23\"."
 
-    def test_get_chains(self):
-        """Check if a new chain can be retrieved."""
+    def test_get_workflows(self):
+        """Check if a new workflow can be retrieved."""
         assert self.client.post("/api/workflows", json=dict(
-            name="First Chain",
-            description="Some foobar chain.",
+            name="First Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-olena-binarize"],
         )).status_code == 201
         assert self.client.post("/api/workflows", json=dict(
-            name="Second Chain",
-            description="Some barfoo chain.",
+            name="Second Workflow",
+            description="Some barfoo workflow.",
             processors=["ocrd-sbb-textline-detector"],
         )).status_code == 201
         response = self.client.get("/api/workflows")
@@ -159,38 +159,38 @@ class ApiTests(TestCase):
         assert response.status_code == 200
         assert len(response.json) == 2
         assert response.json[0]["id"] == 1
-        assert response.json[0]["name"] == "First Chain"
-        assert response.json[0]["description"] == "Some foobar chain."
+        assert response.json[0]["name"] == "First Workflow"
+        assert response.json[0]["description"] == "Some foobar workflow."
         assert "ocrd-olena-binarize" in response.json[0]["processors"]
         assert response.json[1]["id"] == 2
-        assert response.json[1]["name"] == "Second Chain"
-        assert response.json[1]["description"] == "Some barfoo chain."
+        assert response.json[1]["name"] == "Second Workflow"
+        assert response.json[1]["description"] == "Some barfoo workflow."
         assert "ocrd-sbb-textline-detector" in response.json[1]["processors"]
 
-    def test_update_chain(self):
-        """Check if a new chain is created."""
+    def test_update_workflow(self):
+        """Check if a new workflow is created."""
         self.client.post("/api/workflows", json=dict(
-            name="New Chain",
-            description="Some foobar chain.",
+            name="New Workflow",
+            description="Some foobar workflow.",
             processors=["ocrd-tesserocr-recognize"],
         ))
         response = self.client.get("/api/workflows/1")
         assert response.status_code == 200
         assert response.json["id"] == 1
-        assert response.json["name"] == "New Chain"
-        assert response.json["description"] == "Some foobar chain."
+        assert response.json["name"] == "New Workflow"
+        assert response.json["description"] == "Some foobar workflow."
         assert "ocrd-tesserocr-recognize" in response.json["processors"]
 
         response = self.client.put("/api/workflows/1", json=dict(
-            name="Updated Chain",
-            description="Some barfoo chain.",
+            name="Updated Workflow",
+            description="Some barfoo workflow.",
             processors=["ocrd-tesserocr-segment-word"],
         ))
         response = self.client.get("/api/workflows/1")
         assert response.status_code == 200
         assert response.json["id"] == 1
-        assert response.json["name"] == "Updated Chain"
-        assert response.json["description"] == "Some barfoo chain."
+        assert response.json["name"] == "Updated Workflow"
+        assert response.json["description"] == "Some barfoo workflow."
         assert "ocrd-tesserocr-recognize" not in response.json["processors"]
         assert "ocrd-tesserocr-segment-word" in response.json["processors"]
 
