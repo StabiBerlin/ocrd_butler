998944c306410cce4d6d6edb710f899f1c38f46d
Marco Scheidhuber <marco.scheidhuber@sbb.spk-berlin.de>
2022-01-20 10:56:08 +0100

    Remove debugger.

--- tests/test_api_workflow.py
+++ tests/test_api_workflow.py
@@ -205,8 +205,7 @@ class ApiWorkflowTests(TestCase):
         response = self.client.get('/api/workflows')
         assert response.status_code == 200
         assert len(response.json) == 8
-        import ipdb; ipdb.set_trace()
-        assert response.json[0]['description'].startswith('Workflow ')
+        assert response.json[0]['description'].startswith('Basic ')
 
     def test_get_workflows(self):
         """Check if a new workflow can be retrieved."""
